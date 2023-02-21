e773392271141926bdaf97e826bd6476af8ddc69
NeneLawani <nene@lawani.com>
2023-02-21 12:37:52 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: the firs number
+ * @b: the second number
+ * Return: addition of both
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
