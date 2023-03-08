6dbb5206b3b326809d7ad651605ae3d2c14c3278
NeneLawani <nene@lawani.com>
2023-03-08 13:14:50 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: me
+ * Desc: header file containing prototypes for all functions
+ * used in 0x08-recursion directory
+ */
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif
