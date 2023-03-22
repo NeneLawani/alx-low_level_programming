6d0358925b16557fff9b7f7ed7f783ba7931a9f2
NeneLawani <nene@lawani.com>
2023-03-22 11:57:28 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name
+ * @name: name of the person
+ * @f: pointer to the function
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUN_POINT_H
+#define FUN_POINT_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* #ifndef FUN_POINT_H */
