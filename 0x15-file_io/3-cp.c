0d7f9938c7cbf98d62521dde12105c82a98043a6
NeneLawani <nene@lawani.com>
2023-04-10 19:06:42 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * buffer - a function that allocates 1024 bytes on the heap
+ * @argument: the argv file for which memory is being allocated
+ *
+ * Return: a pointer to the allocated buffer
+ */
+char *buffer(char *argument)
+{
+	char *buf;
+
+	buf = malloc(1024 * sizeof(char));
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argument);
+		exit(99);
+	}
+
+	return (buf);
+}
+
+/**
  * close_file - a function which closes file descriptors
  * @fd: the file descriptor to be closed
  */
@@ -23,38 +43,40 @@ void close_file(int fd)
  * @av: a pointer to the arguments
  * Return: 1 (Success); 97, 98, 99, 100 (Failure)
  */
-int main(int ac, char **av)
+int main(int ac, char *av[])
 {
-	int file_from, file_to, wr, rd;
+	int file_from, file_to, rd, wr;
 	char *buf;
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buf = malloc(1024 * sizeof(char));
-	if (buf == NULL)
-		return (99);
+	buf = buffer(av[2]);
 	file_from = open(av[1], O_RDONLY);
 	rd = read(file_from, buf, 1024);
-	if (file_from == -1 || rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		free(buf);
-		exit(98);
-	}
-	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0664);
-	if (file_to == -1)
-		if (errno == EEXIST)
-			file_to = open(av[2], O_WRONLY | O_TRUNC);
-	wr = write(file_to, buf, rd);
-	if (file_to == -1 || wr == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		free(buf);
-		exit(99);
-	}
+	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	do {
+		if (file_from == -1 || rd == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", av[1]);
+			free(buf);
+			exit(98);
+		}
+		wr = write(file_to, buf, rd);
+		if (file_to == -1 || wr == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", av[2]);
+			free(buf);
+			exit(99);
+		}
+		rd = read(file_from, buf, 1024);
+		file_to = open(av[2], O_WRONLY | O_APPEND);
+	} while (rd > 0);
+
 	free(buf);
 	close_file(file_from);
 	close_file(file_to);
---
+++ 0x15-file_io/check/3-cp.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * close_file - a function which closes file descriptors
+ * @fd: the file descriptor to be closed
+ */
+void close_file(int fd)
+{
+	int cl;
+
+	cl = close(fd);
+
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Entry point
+ * @ac: the number of arguments passed
+ * @av: a pointer to the arguments
+ * Return: 1 (Success); 97, 98, 99, 100 (Failure)
+ */
+int main(int ac, char **av)
+{
+	int file_from, file_to, wr, rd;
+	char *buf;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: file_from file_to\n");
+		exit(97);
+	}
+	buf = malloc(1024 * sizeof(char));
+	if (buf == NULL)
+		return (99);
+	file_from = open(av[1], O_RDONLY);
+	rd = read(file_from, buf, 1024);
+	if (file_from == -1 || rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		free(buf);
+		exit(98);
+	}
+	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0664);
+	if (file_to == -1)
+		if (errno == EEXIST)
+			file_to = open(av[2], O_WRONLY | O_TRUNC);
+	wr = write(file_to, buf, rd);
+	if (file_to == -1 || wr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		free(buf);
+		exit(99);
+	}
+	free(buf);
+	close_file(file_from);
+	close_file(file_to);
+	return (0);
+}
