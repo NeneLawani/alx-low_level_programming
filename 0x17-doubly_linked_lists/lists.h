ef8d6a2c6c0d51ed5ef234fb9381dbd49bed1da7
NeneLawani <nene@lawani.com>
2023-05-18 14:25:37 -0700

    7. Insert at index

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,10 +1,10 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
-#ifndef LISTS_H
-#define LISTS_H
 
 /**
  * struct dlistint_s - doubly linked list
@@ -13,14 +13,14 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
